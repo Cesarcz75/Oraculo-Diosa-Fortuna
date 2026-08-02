34998e0d3c78d68beccf4e8b740121238842e78e
Cesarcz75 <cesar.cruz.zeron@gmail.com>
2026-08-01 18:40:09 -0600

    Corregir prueba de inicio de la aplicación

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:oraculo_diosa_fortuna/app/oraculo_app.dart';
 
 void main() {
-  testWidgets('la aplicación inicia correctamente', (WidgetTester tester) async {
+  testWidgets('la aplicación inicia correctamente', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(const OraculoApp());
-    expect(find.text('Oráculo Diosa Fortuna Professional'), findsOneWidget);
+    await tester.pump();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(tester.takeException(), isNull);
   });
 }
