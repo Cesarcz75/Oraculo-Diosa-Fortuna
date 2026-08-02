d7fc54cafc6d4176d8d417de3a21fa244692d484
Cesarcz75 <cesar.cruz.zeron@gmail.com>
2026-08-01 21:49:35 -0600

    Delete widget_test.dart

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:oraculo_diosa_fortuna/app/oraculo_app.dart';
-
-void main() {
-  testWidgets('la aplicación inicia correctamente', (WidgetTester tester) async {
-    await tester.pumpWidget(const OraculoApp());
-    expect(find.text('Oráculo Diosa Fortuna Professional'), findsOneWidget);
-  });
-}
